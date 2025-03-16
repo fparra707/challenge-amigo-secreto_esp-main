0033d8934fd711bfa0904268e22c062258009af3
fparra707 <fparraa@gmail.com>
2025-03-15 22:50:38 -0500

    Se incluye rutinas para validaciones del texto

--- app.js
+++ app.js
@@ -10,57 +10,91 @@ let listaNumerosGenerados = [];             //Array para los número generados a
 function agregarAmigo() {
     
     //Se toma el nombre digitado en el cuadro de texto
-    let nombreAmigo = document.getElementById("amigo").value;
-    
-    //Validamos si el usuario digitó algún nombre
-    if (nombreAmigo === "") {
-        alert("Por favor, ingrese un nombre válido");        
-    } else{
-        //Se agrega el nombre al array listaAmigos en la última posición
-        listaAmigos.push(nombreAmigo);
+    let inputAmigo = document.getElementById("amigo");
+    let nombreAmigo = inputAmigo.value;
 
-        //Creamos el elemento (amigo) de la lista
-        let amigo = document.createElement("li");
-        //amigo.textContent = nombreAmigo;
-        amigo.innerHTML = nombreAmigo;
-        
-        
-        //Incluimos el elemento (amigo) a la lista
-        document.getElementById("listaAmigos").appendChild(amigo);
+    //console.log(nombreAmigo);
+    
+    //Realizamos las siguiente validaciones: Campo vacío?, Solo letras válidas?, se retringe a 35 caracteres
+
+    if(!inputAmigo.checkValidity()) {
+        //alert("El campo no es válido.");
+        alert("Por favor ingrese un nombre válido");
 
         //Limpia cuadro de texto
-        document.getElementById("amigo").value = "";
-        
-        console.log(listaAmigos);
-        console.log(amigo);    
-    }       
+        //document.getElementById("amigo").value = "";
+        inputAmigo.value = "";
+        inputAmigo.focus();     //Ubica el cursor en el cuadro de texto
+
+    } else {
+        //alert("El campo es válido.");
+
+        //Se valida si el nombre ingresado ya se encuentra en el listado
+        if(amigoRepetido(nombreAmigo,listaAmigos) == false){
+
+            //Se agrega el nombre al array listaAmigos en la última posición
+            listaAmigos.push(nombreAmigo);
+            
+            //Creamos el elemento (amigo) de la lista
+            let amigo = document.createElement("li");
+            //amigo.textContent = nombreAmigo;
+            amigo.innerHTML = nombreAmigo;
+                            
+            //Incluimos el elemento (amigo) a la lista
+            document.getElementById("listaAmigos").appendChild(amigo);
+    
+            //Limpia cuadro de texto
+            //document.getElementById("amigo").value = "";
+            inputAmigo.value = "";
+            inputAmigo.focus();     //Ubica el cursor en el cuadro de texto
+            
+            console.log(listaAmigos);
+            console.log(amigo);
+        }else{
+            alert("El nombre ya se encuentra en el listado");
+            inputAmigo.value = "";
+            inputAmigo.focus();     //Ubica el cursor en el cuadro de texto
+        }           
+    }            
 }
+
 
 //Funcion para sortear amigo secreto usando un número aleatorio
 function sortearAmigo(){
+
+    //Se valida si hay listado de amigos
+    if(listaAmigos.length < 3){
+        alert("Deben listarse al menos 3 amigos, por favor ingrese los nombres a sortear");  
+        return;
+    }
+
     //Se genere número aleatorio
     let numeroGenerado = Math.floor(Math.random()*listaAmigos.length);  //El número generado señala la posición en el array de listaAmigos
     //console.log(numeroGenerado);
-    //console.log(listaAmigos[numeroGenerado]);                         //Se valida el nombre seleccionado por el número generado
+    //console.log(listaAmigos[numeroGenerado]);                         
+
+    //Establecemos la variable resultado de acuerdo al html
+    let resultado = document.getElementById("resultado");
 
     //Mostramos el texto con el amigo secreto seleccionado
-    asignarTextoElemento('p',`El amigo secreto es ${listaAmigos[numeroGenerado]}`);
+    resultado.innerHTML = `El amigo secreto es: ${listaAmigos[numeroGenerado]}`;
 
-     //Validamos si se han sorteado todos los amigos
-    if (listaNumerosGenerados.length == listaAmigos.length) {
-        asignarTextoElemento('p','Ya se sortearon todos los amigos posibles');
+    //Limpiamos el listado visualizado en el html
+    let limpiarLista = document.getElementById("listaAmigos");
+    limpiarLista.innerHTML = "";    
+}
 
-    } else {
-        //Si el número generado está incluido en la lista
-        if (listaNumerosGenerados.includes(numeroGenerado)) {
-            return sortearAmigo();     //recursividad (la función se llama a si misma)
-        } else {
-            listaNumerosGenerados.push(numeroGenerado);            
-        }
+
+function amigoRepetido(elemento,array){
+
+    //Validar si el nombre ya está en el array
+    for(i = 0; i < listaAmigos.length; i++){
+        if(elemento === array[i]){
+            //alert("El nombre ya se encuentra en el listado");    
+            return true;
+        }                                    
     }
-
-    console.log(listaNumerosGenerados);
-
+    return false;
 }
 
 //Función para mostrar texto en parrafo seleccionado
